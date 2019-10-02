0a4048b8a8f55395a88dd063513c539fbf887265
Waltero Estoplero <waltser@web.de>
2019-10-02 12:05:14 +0200

    MyFirst Commit

---
+++ FHL-string001/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FHL_string001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "aaa";
+            String b = "bbb";
+            string hallo = "Hallo!";
+            int len_hallo = hallo.Length;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine("hallo hat die Länge " + len_hallo);
+
+            string hello = "Hello ";
+            string name = "Michael";
+            string gesamtstring = hello + name;
+            Console.WriteLine(gesamtstring);
+
+            string gesamtG = gesamtstring.ToUpper();
+            Console.WriteLine(gesamtG);
+
+            gesamtG.Replace("MICHAEL", "Otto");
+            Console.WriteLine(gesamtG);
+
+            gesamtG = gesamtG.Replace("MICHAEL", "Otto");
+            Console.WriteLine(gesamtG);
+
+
+            Console.ReadKey();
+        }
+    }
+}
