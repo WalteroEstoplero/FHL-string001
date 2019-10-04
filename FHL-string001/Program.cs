3ee0d17a9a267a61d52c19108c558d37049f6262
Waltero Estoplero <waltser@web.de>
2019-10-04 11:22:18 +0200

    Änderungen vom Script getestet. String-Modifizierer

--- FHL-string001/Program.cs
+++ FHL-string001/Program.cs
@@ -32,6 +32,53 @@ namespace FHL_string001
             gesamtG = gesamtG.Replace("MICHAEL", "Otto");
             Console.WriteLine(gesamtG);
 
+            string test = "1234567890";
+            int anzahl = test.Length;
+            Console.WriteLine("Die Länge des strings beträgt: " + anzahl);
+
+            string fh = "Einführung in die Programmierung";
+            int first_idx = fh.IndexOf('f');
+            Console.WriteLine("Das erste 'f' wurde gefunden an der Stelle " + first_idx);
+            int last_idx = fh.LastIndexOf('i');
+            Console.WriteLine("Das letzte 'i' wurde gefunden an :" + last_idx);
+
+            string vergl1 = "abcdef";
+            string vergl2 = "abcdef";
+            int vergleich = vergl1.CompareTo(vergl2);
+            //int vergleich = a.CompareTo("1234");
+            if (vergleich == 0) Console.WriteLine(vergl1 + " und " + vergl2 + " sind gleich");
+            else Console.WriteLine(vergl1 + " und " + vergl2 + " sind ungleich");
+
+            vergl2 = "abcde";
+            bool vergleich2 = vergl1.Equals(vergl2);
+            if (vergleich2 == true) Console.WriteLine(vergl1 + " und " + vergl2 + " sind gleich");
+            else Console.WriteLine(vergl1 + " und " + vergl2 + " sind ungleich");
+
+
+            string[] gesplittet = fh.Split(' ');
+            foreach (string item in gesplittet)
+            {
+                Console.WriteLine(item);
+            }
+
+            string fh_leibniz = fh.Insert(0, "Leibniz FH - ");
+            Console.WriteLine(fh_leibniz);
+
+            string removed = fh_leibniz.Remove(0, 8);
+            Console.WriteLine(removed);
+
+            int temperatur = 20;
+            string temp_s = temperatur.ToString();
+
+
+
+            /*
+                        string nachricht = "Neue Nachricht";
+                        string name2 = "Helga";
+            // ERROR            name2.Append(" hat eine Nachricht bekommen: ");
+            // ERROR            name2.Append(nachricht);
+            */
+
 
             Console.ReadKey();
         }
